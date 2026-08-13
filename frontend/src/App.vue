15cce1dd7f84f33f0a259609e6c9cd3da2a00e9d
Dicun <buicuong7954@gmail.com>
2026-08-13 15:07:03 +0700

    fix not use in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { computed, watch } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 
 import AppLayout from '@/app/layouts/AppLayout.vue'
 import AuthLayout from '@/app/layouts/AuthLayout.vue'
